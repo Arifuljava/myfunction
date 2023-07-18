efc0513b95ce7d5676411ccac24f41eb73e85800
Arif's Zone <arifulislam.java.c@gmail.com>
2023-07-17 23:22:54 -0700

    main

--- lib/createmanyqrcode.dart
+++ lib/createmanyqrcode.dart
@@ -1,52 +1,115 @@
+﻿
 
-
-import 'dart:ffi';
-
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+int flag=0;
 
-class createmanyqrcode extends StatefulWidget {
-  const createmanyqrcode({super.key});
+class CreateManyQRCode extends StatefulWidget {
+  const CreateManyQRCode({Key? key}) : super(key: key);
 
   @override
-  State<createmanyqrcode> createState() => _createmanyqrcodeState();
+  _CreateManyQRCodeState createState() => _CreateManyQRCodeState();
 }
 
-class _createmanyqrcodeState extends State<createmanyqrcode> {
+class _CreateManyQRCodeState extends State<CreateManyQRCode> {
   List<String> qrCodes = [];
+  List<Offset> qrCodeOffsets = [];
+  GlobalKey _widgetKey = GlobalKey();
+  List<double> widgetWidths = [];
+  List<double> widgetHeights = [];
+  List<int> widgetWidths22 = [];
+
+  void measureWidget(int index) {
+    final RenderBox? widgetBox =
+    _widgetKey.currentContext?.findRenderObject() as RenderBox?;
+    if (widgetBox != null) {
+      setState(() {
+        widgetWidths[index] = widgetBox.size.width;
+        widgetHeights[index] = widgetBox.size.height;
+      });
+    }
+  }
+
 
   void generateQRCode() {
     setState(() {
       qrCodes.add('QR Code ${qrCodes.length + 1}');
+      widgetWidths22.add(50);
+      qrCodeOffsets.add(Offset(0, (qrCodes.length * 5).toDouble()));
+    });
+  }
+
+  void updateQRCodeOffset(int index, Offset offset) {
+    setState(() {
+      qrCodeOffsets[index] = offset;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-   return MaterialApp(
-     home: Scaffold(
-       appBar: AppBar(
-         title: Text("Many Qr Code"),
-       ),
-       body: Center(
-         child: Row(
-           mainAxisAlignment: MainAxisAlignment.center,
-           children: <Widget>[
-             ElevatedButton(onPressed: (){
-               generateQRCode();
-             }, child: Text("Create Qr Code")),
-             SizedBox(height: 10,),
-             Column(
-               children: qrCodes.map((qrCode) => QrImageView(data: qrCode,size: 40,)).toList(),
-             )
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Many QR Codes"),
+        ),
+        body: Center(
+          child: Stack(
+            children: <Widget>[
+              for (var i = 0; i < qrCodes.length; i++)
+                Positioned(
+                  left: qrCodeOffsets[i].dx,
+                  top: qrCodeOffsets[i].dy,
+                  child: Draggable(
+                    child: QrImageView(
+                      key: UniqueKey(),
+                      data: qrCodes[i],
+                      size:  50.00,
+                    ),
+                    feedback: QrImageView(
+                      data: qrCodes[i],
+                      size: 50,
+                    ),
+                    onDraggableCanceled: (velocity, offset) {
+                      updateQRCodeOffset(i, offset);
+                    },
+                  ),
+                ),
+              Positioned(
+                bottom: 70,
+                child: ElevatedButton(
+                  onPressed: (){
+                    generateQRCode();
+                    flag=1;
+                  },
+                  child: Text("Create QR Code"),
+                ),
+              ),
+              Positioned(
+                bottom: 10,
+                child: ElevatedButton(
+                  onPressed: (){
+                    print(qrCodes.length);
+                    print(flag);
+                    for (var j = 0; j < qrCodes.length; j++)
+                      {
+                        print(qrCodes[j]);
+                        print(qrCodeOffsets[j].dx);
+                        print(qrCodeOffsets[j].dy);
+                        print(qrCodeOffsets[j].dx);
 
-           ],
-         ),
-       ),
-     ),
-   );
+                      }
+
+
+                  },
+                  child: Text("Save Information"),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
+
